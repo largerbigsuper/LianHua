fdd12c575701a5e2dbb09c3a9e24350bc2eb33ad
turkey <zaihuazhao@163.com>
2019-05-15 21:40:29 +0800

    fix midea root

--- LianHua/settings.py
+++ LianHua/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'auth.User'
