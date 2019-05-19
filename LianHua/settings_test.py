f28badfbdfebfefd32fa532f7d6688527d6845ea
turkey <zaihuazhao@163.com>
2019-05-19 15:26:27 +0800

    feature test server suport https

--- LianHua/settings_test.py
+++ LianHua/settings_test.py
@@ -41,3 +41,11 @@ class QiNiuSettings:
     BUCKET_DOMAIN_DICT = {
         'image': 'http://lhxq.top/'
     }
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = ['servicewechat.com', 'lianhua..lhxq.top', 'lhxq.top']
